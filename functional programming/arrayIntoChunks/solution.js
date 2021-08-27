f1eacf4a6c8ee2f41bde084f6b264009f7fdbb42
Piotr Grzybowski <pgrzybowski.dev@gmail.com>
2021-08-27 15:44:23 +0200

    Add aggregateArrayIntoChunks solution

--- functional programming/arrayIntoChunks/solution.js
+++ functional programming/arrayIntoChunks/solution.js
@@ -3,13 +3,14 @@
 * For example for arrayLength equal to 10 one of possible result is [4, 6]
 * It means that array should be splitted into two chunks. First 4 elements long and other 6 elements long
  */
-function findChunksLength (arrayLength) {
+function findChunksLength(arrayLength) {
   let i = 0;
   let chunksLengths = [];
   while (i < arrayLength) {
-    let chunkLength = Math.floor(Math.random() * 4 + 4);
+    let chunkLength = Math.floor(Math.random() * 4 + 4); // finds random number between 4 and 7
     i += chunkLength;
     chunksLengths.push(chunkLength);
+    // if random numbers of chunks lengths is higher then array length we have to start loop again
     if (i > arrayLength) {
       chunksLengths = [];
       i = 0;
@@ -22,8 +23,17 @@ function findChunksLength (arrayLength) {
 * It returns new array that contains chunks of original input array
  */
 function aggregateArrayIntoChunks(array) {
-
+  const chunksLengths = findChunksLength(array.length);
+  const arrayCopy = [...array];
+  for (let element of chunksLengths) {
+    let chunk = arrayCopy.splice(0, element);
+    arrayCopy.push(chunk);
+  }
+  return arrayCopy;
 }
 
 
-findChunksLength(21);
+const alphabet = "abcdefghijklmnoprstuwxyz".split("");
+
+aggregateArrayIntoChunks(alphabet);
+
