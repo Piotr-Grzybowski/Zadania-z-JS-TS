9a23b80563a764da86f55827413ea78ca23af5f0
Piotr Grzybowski <pgrzybowski.dev@gmail.com>
2021-08-27 14:58:13 +0200

    Add aggregateIntoChunks task

---
+++ functional programming/arrayIntoChunks/README.md
@@ -0,0 +1,18 @@
+### Create function **aggregateIntoChunks** that will aggregate all elements of array into random length chunks
+
+- [ ] chunks should be between 4 to 7 elements long
+- [ ] length of last chunk also should be from 4 to 7 elements
+
+
+```javascript
+const alphabet = "abcdefghijklmnoprstuwxyz".split("");
+
+const aggregateIntoChunks = (array) => {
+  // return ...
+};
+
+const chunks = aggregateIntoChunks(alphabet);
+
+// chunks:
+// [[a,b,c,d,e,f],[g,h,i,j,k],[l,m,n,o,p,r,s],[t,u,w,x,y,z]]
+```
---
+++ functional programming/arrayIntoChunks/solution.js
@@ -0,0 +1,15 @@
+/**
+* Function that given length of array will return an array of random chunks lengths between 4 and 7
+* For example for arrayLength equal to 10 one of possible result is [4, 6]
+* It means that array should be splitted into two chunks. First 4 elements long and other 6 elements long
+ */
+function findChunksLength (arrayLength) {
+
+}
+/**
+* Function takes an array and using above findsChunksLength function aggregate array into chunks from 4 to 7 elements long
+* It returns new array that contains chunks of original input array
+ */
+function aggregateArrayIntoChunks(array) {
+
+}
