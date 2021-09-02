ee4db39ee8bd178dda8c927f47eeb39c29213b5b
Piotr Grzybowski <pgrzybowski.dev@gmail.com>
2021-09-02 20:03:46 +0200

    Add solution of filterWith

---
+++ functional programming/filterWith/solution.js
@@ -0,0 +1,158 @@
+function filterWith(array, phrase) {
+  if (typeof phrase === "string") {
+    if (phrase.length <= 2) return [];
+  }
+  return array.filter(element => {
+    const values = Object.values(element);
+    const result = findElement(values, phrase);
+    if (result.length > 0) return true;
+    return false;
+  })
+}
+/**
+* It's a function that given array of elements and phrase to  be found will return element searched for
+* It's recursive what means a function calls itself until it doesn’t.
+ */
+function findElement(array, phrase) {
+  const regex = new RegExp(phrase.toString().toLowerCase());
+  return array.filter(element => {
+    if (typeof(element) === 'object') {
+      if (Array.isArray(element)) {
+        return findElement(element, phrase).length;
+      }
+      return findElement(Object.values(element), phrase).length;
+    }
+    if (typeof(element) === 'string') return regex.test(element.toLowerCase());
+    if (typeof(element) === 'number') return regex.test(element.toString().toLowerCase());
+    return false;
+  })
+}
+
+// Example data for practice
+const data = [
+  {
+    _id: "5e985a07feddae7617ac44f6",
+    age: 24,
+    eyeColor: "brown",
+    name: "Cummings Baxter",
+    gender: "male",
+    company: "VELOS",
+    email: "cummingsbaxter@velos.com",
+    phone: "+1 (907) 482-2451",
+    tags: ["labore", "elit", "excepteur", "nisi", "mollit", "anim", "aliquip"],
+    friends: [
+      {
+        id: 0,
+        name: "Sheppard Jensen",
+      },
+    ],
+  },
+  {
+    _id: "5e985a0709dfa1e6fd93c6ad",
+    age: 32,
+    eyeColor: "brown",
+    name: "Madelyn Dickson",
+    gender: "female",
+    company: "KENGEN",
+    email: "madelyndickson@kengen.com",
+    phone: "+1 (984) 521-2439",
+    tags: ["nisi", "veniam", "dolore", "officia", "ex", "non", "pariatur"],
+    friends: [
+      {
+        id: 0,
+        name: "Bruce Barton",
+      },
+      {
+        id: 1,
+        name: "Juliet Schmidt",
+      },
+      {
+        id: 2,
+        name: "Horton Haley",
+      },
+      {
+        id: 3,
+        name: "Herminia Witt",
+      },
+    ],
+  },
+  {
+    _id: "5e985a0737e2306e9aef6ecd",
+    age: 26,
+    eyeColor: "blue",
+    name: "Mcguire Mercado",
+    gender: "male",
+    company: "LINGOAGE",
+    email: "mcguiremercado@lingoage.com",
+    phone: "+1 (963) 450-2194",
+    tags: ["cupidatat", "occaecat", "amet", "qui", "elit", "esse", "deserunt"],
+    friends: [
+      {
+        id: 0,
+        name: "Loraine Harper",
+      },
+      {
+        id: 1,
+        name: "Luann Randall",
+      },
+      {
+        id: 2,
+        name: "Obrien Rich",
+      },
+      {
+        id: 3,
+        name: "Noble Wilkerson",
+      },
+    ],
+  },
+  {
+    _id: "5e985a07148cfba58c860ec2",
+    age: 26,
+    eyeColor: "brown",
+    name: "Marina Porter",
+    gender: "female",
+    company: "GORGANIC",
+    email: "marinaporter@gorganic.com",
+    phone: "+1 (867) 417-3497",
+    tags: ["laborum", "aliquip", "sit", "adipisicing", "aute", "cupidatat", "aliquip"],
+    friends: [
+      {
+        id: 0,
+        name: "Blair Hill",
+      },
+      {
+        id: 1,
+        name: "Ebony Jimenez",
+      },
+    ],
+  },
+  {
+    _id: "5e985a074984f9f08ccaaa4c",
+    age: 25,
+    eyeColor: "green",
+    name: "Barlow Ferguson",
+    gender: "male",
+    company: "TOYLETRY",
+    email: "barlowferguson@toyletry.com",
+    phone: "+1 (837) 484-2231",
+    tags: ["est", "dolor", "minim", "ut", "anim", "culpa", "non"],
+    friends: [
+      {
+        id: 0,
+        name: "Delacruz Acevedo",
+      },
+      {
+        id: 1,
+        name: "Gloria Tanner",
+      },
+      {
+        id: 2,
+        name: "Cantrell Myers",
+      },
+      {
+        id: 3,
+        name: "Fisher Leonard",
+      },
+    ],
+  },
+];
