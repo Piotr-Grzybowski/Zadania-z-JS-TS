2121bdf22a35ce6ec8395f5647e39889a705dbeb
Piotr Grzybowski <pgrzybowski.dev@gmail.com>
2021-11-01 13:54:29 +0100

    Add user class

---
+++ Object-oriented programming (OOP)/User/User.js
@@ -0,0 +1,36 @@
+import { Validator } from '../utils/Validator.js';
+import {uuidv4} from '../utils/utils.js';
+
+class User {
+  constructor({ firstName, lastName, dateOfBirth, password, sex, email }, accessLevel = 'user') {
+    Validator.isNotEmpty(firstName, 'First name');
+    Validator.isNotEmpty(lastName, 'Last name');
+    Validator.isCorrectDateFormat(dateOfBirth);
+    Validator.isPasswordCorrect(password);
+    Validator.isMaleOrFemale(sex);
+    Validator.isEmail(email);
+
+    this.uuid = uuidv4();
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
+    this.password = password;
+    this.sex = sex;
+    this.email = email;
+    this.accessLevel = accessLevel;
+  }
+  changePassword = function(password) {
+    Validator.isPasswordCorrect(password);
+    this.password = password;
+  }
+  changeEmail = function(email) {
+    Validator.isEmail(email);
+    this.email = email;
+  }
+  changeAccessLevel = function(accessLevel) {
+    Validator.isAccessLevel(accessLevel);
+    this.accessLevel = accessLevel;
+  }
+}
+
+export default User;
