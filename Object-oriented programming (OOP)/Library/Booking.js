37a47330dc0912596d86cc35a75097a52613db95
Piotr Grzybowski <pgrzybowski.dev@gmail.com>
2021-11-06 21:35:45 +0100

    Finish class Booking

--- Object-oriented programming (OOP)/Library/Booking.js
+++ Object-oriented programming (OOP)/Library/Booking.js
@@ -1,22 +1,27 @@
 import User from "./User.js";
 
 class Booking {
-  constructor(userId, listOfBooks, penalty = 10) {
+  constructor(userId, listOfBooks, penaltyPerDay = 10) {
     if (listOfBooks.length === 0) throw new Error('List of borrowed books can\'t be empty');
     this.user = userId;
     this.listOfBorrowedBooks = listOfBooks;
-    this.penalty = penalty;
+    this.penaltyPerDay = penaltyPerDay;
     this.dateOfBorrow = new Date();
     this.dateOfReturn = new Date(this.dateOfBorrow.getTime() + (7 * (86400000))); // 86400000 is one day (24 hours) in milliseconds
+    this.totalPenaltyToPay = 0;
   }
   addBookToListOfBorrowed = function(bookId) {
     if (!this.dateOfBorrow.getDate() === new Date().getDate()) throw new Error('You can add new books to the booking only in the same day booking started');
     this.listOfBorrowedBooks.push(bookId);
   }
-  returnABook = function(bookId) {
+  removeBookFromListOfBorrowed = function(bookId) {
     const indexOfBook = this.getIndexOfBook(bookId);
-    const penalty = this.calculatePenalty();
     this.listOfBorrowedBooks.splice(indexOfBook, 1);
+  }
+  returnABook = function(bookId) {
+    this.removeBookFromListOfBorrowed(bookId);
+    const penalty = this.calculatePenalty();
+    this.totalPenaltyToPay += penalty;
   }
   checkIfBookExist = function(bookId) {
     if (this.listOfBorrowedBooks.indexOf(bookId) !== -1) return true;
@@ -28,7 +33,11 @@ class Booking {
   }
   calculatePenalty = function() {
     const today = new Date();
-
+    if (today > this.dateOfReturn) {
+      const days = Math.floor((today.getTime() - this.dateOfReturn.getTime()) / 86400000); // 86400000 is one day (24 hours) in milliseconds
+      return days * this.penalty;
+    }
+    return 0;
   }
 }
 
